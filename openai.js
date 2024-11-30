d661d48f5a2004306e035d198fe8bc0713dfba75
Yahia Aly <79812056+YahiaMH@users.noreply.github.com>
2024-11-30 10:53:50 +0000

    changed model to gpt-4o

--- openai.js
+++ openai.js
@@ -1,16 +1,16 @@
-const OpenAI = require('openai');
+const OpenAI = require("openai");
 
 const openai = new OpenAI({
-	apiKey: process.env.OPENAI_API_KEY
+	apiKey: process.env.OPENAI_API_KEY,
 });
 
-module.exports = async (messages, model = "gpt-4") => {
+module.exports = async (messages, model = "gpt-4o") => {
 	try {
 		const chatCompletion = await openai.chat.completions.create({
 			model,
 			messages,
 		});
-		
+
 		return chatCompletion.choices[0].message.content;
 	} catch (err) {
 		console.log(err);

9ba59fb45b288f09a9e379940be48a8f5db15647
Yahia Aly <79812056+YahiaMH@users.noreply.github.com>
2024-11-30 10:59:14 +0000

    Cleaned up index.js

--- index.js
+++ index.js
@@ -1,74 +1,82 @@
-const Discord = require('discord.js');
+const express = require("express");
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const keepAlive = require('./server');
-const fs = require('fs').promises;
-const prefix = '.';
-const path = require('path');
-// const DisTube = require('distube');
-const mongoose = require('mongoose');
-const User = require('./schemas/UserSchema')
-const BJ = require('./schemas/BlackjackSchema')
+const fs = require("fs").promises;
+const path = require("path");
+const mongoose = require("mongoose");
+const User = require("./schemas/UserSchema");
+const BJ = require("./schemas/BlackjackSchema");
 
-client.on('ready', (newMember) => {
-	console.log(`Logged in as ${client.user.tag}!`);
+const prefix = ".";
+
+const server = express();
+
+// Health Check Route
+server.all("/", (req, res) => {
+	res.send("Bot is running!");
+});
+
+// Start the server on the specified port
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+	console.log(`Server is ready on port ${PORT}.`);
 });
 
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
 
 client.commands = new Map();
 
-// client.distube = new DisTube(client, {
-// 	searchSongs: false,
-// 	emitNewSongOnly: true
-// });
-
-(async function registerCommands(dir = 'commands') {
+(async function registerCommands(dir = "commands") {
 	let files = await fs.readdir(path.join(__dirname, dir));
 	for (let file of files) {
 		let stat = await fs.lstat(path.join(__dirname, dir, file));
 		if (stat.isDirectory()) registerCommands(path.join(dir, file));
 		else {
-			if (file.endsWith('.js')) {
-				let cmdName = file.substring(0, file.indexOf('.js'));
+			if (file.endsWith(".js")) {
+				let cmdName = file.substring(0, file.indexOf(".js"));
 				let cmdModule = require(path.join(__dirname, dir, file));
 				let { aliases } = cmdModule;
 				client.commands.set(cmdName, cmdModule);
 				if (aliases.length !== 0)
-					aliases.forEach(alias => client.commands.set(alias, cmdModule.run));
+					aliases.forEach((alias) => client.commands.set(alias, cmdModule.run));
 			}
 		}
 	}
 })();
 
-client.on('message', async function(message) {
+client.on("ready", () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", async function (message) {
 	if (message.author.bot) return;
-  const target = message.mentions.users.first() || message.author;
-  targetId = target.id;
+	const target = message.mentions.users.first() || message.author;
+	const targetId = target.id;
 
 	if (!message.content.startsWith(prefix)) return;
 	let cmdArgs = message.content
 		.substring(message.content.indexOf(prefix) + 1)
-		.split(new RegExp(/\s+/));
+		.split(/\s+/);
 	let cmdName = cmdArgs.shift();
 	if (client.commands.get(cmdName)) {
 		client.commands.get(cmdName)(client, message, cmdArgs);
 	} else {
-		console.log('Command does not exist');
+		console.log("Command does not exist");
 	}
 });
 
-client.on('guildMemberAdd', async (member) =>{
-  const newMember = await User.create({
-    username: member.user.username,
-    discordId: member.id,
-  });
+client.on("guildMemberAdd", async (member) => {
+	const newMember = await User.create({
+		username: member.user.username,
+		discordId: member.id,
+	});
 	const newBJ = await BJ.create({
-		discordId: member.id
-	})
+		discordId: member.id,
+	});
 });
 
-keepAlive();
+// Login to Discord
 client.login(process.env.TOKEN);
